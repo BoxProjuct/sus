1fa644a68ae45d90ebb10af2f6cba27d681d5b51
BoxBoxBoxBoxBox <110622134+BoxBoxBoxBoxBox@users.noreply.github.com>
2022-10-04 19:38:18 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,14 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NFS-project/local_manifest --depth 1 -b rosy-derp-13 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
+git clone https://gitlab.com/R9Lab/Manifest.git --depth 1 -b CherishOS-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-export NFS_TARGET_BUILD_ROM=DerpFest
 source build/envsetup.sh
-lunch derp_rosy-userdebug
-export TZ=Asia/Jakarta
-export KBUILD_BUILD_USER=rosy
-export KBUILD_BUILD_HOST=nfsproject
-export BUILD_USERNAME=rosy
-export BUILD_HOSTNAME=nfsproject
-export USE_NFSparts=true
-mka derp -j8
+lunch cherish_lava-userdebug
+export WITH_GMS=true
+export TARGET_USES_MINI_GAPPS=true
+export SELINUX_IGNORE_NEVERALLOWS=true
+export TZ=Asia/Dhaka #put before last build command
+mka bacon
 # end
